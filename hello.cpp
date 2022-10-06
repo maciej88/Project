8826327b3c6001ceb0c60607ff3e0e9f3362564a
maciej88 <mkrywult88@gmail.com>
2022-10-06 14:51:06 +0200

    Coments in file

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
+/*
+Project for tutorial of C++ language
+*/
 # include <cstdio>
 int main()
 {
+    // First Hello world in C++
     printf("Hello world!\n");
+    // My lovely City
     printf("Bielsko-biala\n");
+    // My hobby :)
     printf("My hobby is programing!");
 }
