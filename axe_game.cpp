000bf26a81724eb4a4d3466c86606c2cd8a06129
maciej88 <mkrywult88@gmail.com>
2022-10-07 18:04:47 +0200

    Window

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,23 +1,15 @@
 #include "raylib.h"
 int main()
 {
-    float root_beer = 1.99;
-    // false
-    bool equal{ 4 == 9 };
-    // true
-    bool notEqual{ 4 != 9 };
-    // true
-    bool less{ 4 < 9 };
-    // false
-    bool greater{ 4 > 9 };
-    // true
-    bool lessEqual{ 4 <= 9 };
-    // false
-    bool greaterEqual{ 4 >= 9 };
-
     int width;
     width = 350;
-    InitWindow(width, 1200, "Maciej's window");
+    InitWindow(width, 200, "Maciej's window");
 
-
+    while ( true )
+    {
+        BeginDrawing();
+        ClearBackground( RED );
+        EndDrawing();
+    }
+    
 }
