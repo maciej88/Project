89bbac3b83dc449a69f66719535f303228fa4a17
maciej88 <mkrywult88@gmail.com>
2022-10-08 11:04:38 +0200

    coments + key movement "D"

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,16 +1,29 @@
 #include "raylib.h"
 int main()
 {
+    // window dimensions
     int width;
     width = 350;
     InitWindow(width, 200, "Maciej's window");
+
+    //circle coordinates
+    int circle_X{175};
+    int circle_y{100};
+
 
     while ( WindowShouldClose() == false )
     {
         BeginDrawing();
         ClearBackground( WHITE );
 
-        DrawCircle(175, 100, 25, BLUE);
+        // starting game logic
+        DrawCircle(circle_X, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_X += 10;
+        }
+        // ending game logic
         EndDrawing();
     }
     
