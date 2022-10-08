13676dd81e7e1b5676fbd6d87b7f0f42b745551d
maciej88 <mkrywult88@gmail.com>
2022-10-08 11:23:27 +0200

    rectangle

--- axe_game.cpp
+++ axe_game.cpp
@@ -9,6 +9,10 @@ int main()
     //circle coordinates
     int circle_X{175};
     int circle_y{100};
+    int rectangle_x(300);
+    int rectangle_y(0);
+    int rectangle_width(50);
+    int rectangle_hight(50);
 
     SetTargetFPS(60);
     while ( WindowShouldClose() == false )
@@ -18,6 +22,7 @@ int main()
 
         // starting game logic
         DrawCircle(circle_X, circle_y, 25, BLUE);
+        DrawRectangle(rectangle_x, rectangle_y, rectangle_hight, rectangle_width, RED);
 
         if (IsKeyDown(KEY_D) && circle_X < 350)
         {
@@ -27,6 +32,7 @@ int main()
         {
             circle_X -= 10;
         }
+
         // ending game logic
         EndDrawing();
     }
