67592b51843452362e5dc3450d58b779e7e0025c
maciej88 <mkrywult88@gmail.com>
2022-10-09 16:19:59 +0200

    hitboxes

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,14 +7,25 @@ int main()
     InitWindow(width, height, "Maciej's window");
 
     //circle coordinates
-    int circle_X{200};
+    int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+    // circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
 
     // axe cordinates
     int axe_x(400);
     int axe_y(0);
-    int axe_width(50);
-    int axe_height(50);
+    int axe_lenght{50};
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_lenght};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_lenght};
 
     int direction{10};
 
@@ -25,8 +36,8 @@ int main()
         ClearBackground( WHITE );
 
         // starting game logic
-        DrawCircle(circle_X, circle_y, 25, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_height, axe_width, RED);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_lenght, axe_lenght, RED);
 
         // axe movement
         axe_y += direction;
@@ -36,13 +47,13 @@ int main()
         }
 
         // controls
-        if (IsKeyDown(KEY_D) && circle_X < width)
+        if (IsKeyDown(KEY_D) && circle_x < width)
         {
-            circle_X += 10;
+            circle_x += 10;
         }
-        if (IsKeyDown(KEY_A) && circle_X > 0)
+        if (IsKeyDown(KEY_A) && circle_x > 0)
         {
-            circle_X -= 10;
+            circle_x -= 10;
         }
 
         // ending game logic
